0aaefd6dde8e433904743765aebd0ecc14110392
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-11-03 18:56:07 +0100

    fix database drop field error for required fields when formhybrid runonce.php run: loadDataContainerHook only triggers if $GLOBALS[TL_DCA][$strName] is not null

--- classes/Hooks.php
+++ classes/Hooks.php
@@ -5,172 +5,227 @@ namespace HeimrichHannot\Slick;
 class Hooks extends \Controller
 {
 
-	private static $strSpreadDca = 'tl_slick_spread';
+    private static $strSpreadDca = 'tl_slick_spread';
 
-	public function hookReplaceDynamicScriptTags($strBuffer)
-	{
-		global $objPage;
+    public function hookReplaceDynamicScriptTags($strBuffer)
+    {
+        global $objPage;
 
-		if(!$objPage) return $strBuffer;
+        if (!$objPage)
+        {
+            return $strBuffer;
+        }
 
-		$objConfigurations = SlickConfigModel::findAll();
+        $objConfigurations = SlickConfigModel::findAll();
 
-		if($objConfigurations === null)
-		{
-			return $strBuffer;
-		}
+        if ($objConfigurations === null)
+        {
+            return $strBuffer;
+        }
 
-		while ($objConfigurations->next())
-		{
-			SlickConfig::createConfigJs($objConfigurations->current());
-		}
+        while ($objConfigurations->next())
+        {
+            SlickConfig::createConfigJs($objConfigurations->current());
+        }
 
-		return $strBuffer;
-	}
-
-
-	public function parseArticlesHook(&$objTemplate, $arrArticle, $objModule)
-	{
-		if (!$arrArticle['addGallery']) return;
-
-		$objArchive = \NewsArchiveModel::findByPk($arrArticle['pid']);
-
-		if ($objArchive === null) return;
-
-		$objConfig = SlickConfigModel::findByPk($objArchive->slickConfig);
-
-		if ($objConfig === null) return;
-
-		// set size from module
-		$arrArticle['slickSize'] = $objModule->imgSize;
-
-		$objGallery = new Slick(Slick::createSettings($arrArticle, $objConfig));
-
-		$objTemplate->gallery = $objGallery->parse();
-	}
+        return $strBuffer;
+    }
 
 
-	/**
-	 * Spread Fields to existing DataContainers
-	 * @param string $strName
-	 * @return boolean false if Datacontainer not supported
-	 */
-	public function loadDataContainerHook($strName)
-	{
-		if (!is_array($GLOBALS['TL_SLICK']['SUPPORTED']) || !in_array($strName, array_keys($GLOBALS['TL_SLICK']['SUPPORTED']))) return false;
+    public function parseArticlesHook(&$objTemplate, $arrArticle, $objModule)
+    {
+        if (!$arrArticle['addGallery'])
+        {
+            return;
+        }
 
-		\Controller::loadDataContainer(static::$strSpreadDca);
+        $objArchive = \NewsArchiveModel::findByPk($arrArticle['pid']);
 
-		if (!is_array($GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'])) return false;
+        if ($objArchive === null)
+        {
+            return;
+        }
 
-		$dc = &$GLOBALS['TL_DCA'][$strName];
+        $objConfig = SlickConfigModel::findByPk($objArchive->slickConfig);
 
-		foreach ($GLOBALS['TL_SLICK']['SUPPORTED'][$strName] as $strPalette => $replace) {
+        if ($objConfig === null)
+        {
+            return;
+        }
 
-			$arrFields    = array();
-			$arrFieldKeys = array();
-			$arrSelectors = array();
-			$arrSubFields = array();
+        // set size from module
+        $arrArticle['slickSize'] = $objModule->imgSize;
 
-			preg_match_all('#\[\[(?P<constant>.+)\]\]#', $replace, $matches);
+        $objGallery = new Slick(Slick::createSettings($arrArticle, $objConfig));
 
-			if (!isset($matches['constant'][0])) continue;
+        $objTemplate->gallery = $objGallery->parse();
+    }
 
-			$strConstant       = $matches['constant'][0];
-			$strReplacePalette = @constant($matches['constant'][0]);
 
-			$pos    = strpos($replace, '[[' . $strConstant . ']]');
-			$search = str_replace('[[' . $strConstant . ']]', '', $replace);
+    /**
+     * Spread Fields to existing DataContainers
+     *
+     * @param string $strName
+     *
+     * @return boolean false if Datacontainer not supported
+     */
+    public function loadDataContainerHook($strName)
+    {
+        if (!is_array($GLOBALS['TL_SLICK']['SUPPORTED']) || !in_array($strName, array_keys($GLOBALS['TL_SLICK']['SUPPORTED'])))
+        {
+            return false;
+        }
 
-			// prepend slick config palette
-			if ($pos < 1) {
-				$replace = $GLOBALS['TL_DCA'][static::$strSpreadDca]['palettes'][$strReplacePalette] . $search;
-			} // append slick config palette
-			else {
-				$replace = $search . $GLOBALS['TL_DCA'][static::$strSpreadDca]['palettes'][$strReplacePalette];
-			}
+        \Controller::loadDataContainer(static::$strSpreadDca);
 
-			$arrFields = static::getPaletteFields($strReplacePalette, $dc);
+        if (!is_array($GLOBALS['TL_DCA'][static::$strSpreadDca]['fields']))
+        {
+            return false;
+        }
 
-			$arrFieldKeys = array_keys($arrFields);
+        $dc = &$GLOBALS['TL_DCA'][$strName];
 
-			// inject palettes
-			// create palette if not existing
-			if (!isset($dc['palettes'][$strPalette])) {
-				$dc['palettes'][$strPalette] = $replace;
-			} else {
-				$dc['palettes'][$strPalette] = str_replace($search, $replace, $dc['palettes'][$strPalette]);
-			}
+        if($dc === null) return;
 
-			// inject subplattes & selectors
-			$arrSelectors = array_intersect($GLOBALS['TL_DCA'][static::$strSpreadDca]['palettes']['__selector__'], $arrFieldKeys);
+        foreach ($GLOBALS['TL_SLICK']['SUPPORTED'][$strName] as $strPalette => $replace)
+        {
 
-			if (!empty($arrSelectors)) {
-				$dc['palettes']['__selector__'] = array_merge(is_array($dc['palettes']['__selector__']) ? $dc['palettes']['__selector__'] : array(), $arrSelectors);
+            $arrFields    = array();
+            $arrFieldKeys = array();
+            $arrSelectors = array();
+            $arrSubFields = array();
 
-				foreach ($arrSelectors as $key) {
-					$arrFields = array_merge($arrFields, static::getPaletteFields($key, $dc, 'subpalettes'));
+            preg_match_all('#\[\[(?P<constant>.+)\]\]#', $replace, $matches);
 
-				}
+            if (!isset($matches['constant'][0]))
+            {
+                continue;
+            }
 
-				$dc['subpalettes'] = array_merge(is_array($dc['subpalettes']) ? $dc['subpalettes'] : array(), $GLOBALS['TL_DCA'][static::$strSpreadDca]['subpalettes']);
-			}
+            $strConstant       = $matches['constant'][0];
+            $strReplacePalette = @constant($matches['constant'][0]);
 
-			// inject fields
-			$dc['fields'] = array_merge($arrFields, $dc['fields']);
-		}
+            $pos    = strpos($replace, '[[' . $strConstant . ']]');
+            $search = str_replace('[[' . $strConstant . ']]', '', $replace);
 
-		\System::loadLanguageFile(static::$strSpreadDca);
+            // prepend slick config palette
+            if ($pos < 1)
+            {
+                $replace = $GLOBALS['TL_DCA'][static::$strSpreadDca]['palettes'][$strReplacePalette] . $search;
+            } // append slick config palette
+            else
+            {
+                $replace = $search . $GLOBALS['TL_DCA'][static::$strSpreadDca]['palettes'][$strReplacePalette];
+            }
 
-		// add language to TL_LANG palette
-		$GLOBALS['TL_LANG'][$strName] = array_merge($GLOBALS['TL_LANG'][$strName], $GLOBALS['TL_LANG'][static::$strSpreadDca]);
-	}
+            $arrFields = static::getPaletteFields($strReplacePalette, $dc);
 
-	protected static function getPaletteFields($strPalette, $dc, $type = 'palettes')
-	{
-		$boxes = trimsplit(';', $GLOBALS['TL_DCA'][static::$strSpreadDca][$type][$strPalette]);
+            $arrFieldKeys = array_keys($arrFields);
 
-		$arrFields = array();
+            // inject palettes
+            // create palette if not existing
+            if (!isset($dc['palettes'][$strPalette]))
+            {
+                $dc['palettes'][$strPalette] = $replace;
+            } else
+            {
+                $dc['palettes'][$strPalette] = str_replace($search, $replace, $dc['palettes'][$strPalette]);
+            }
 
-		if (!empty($boxes)) {
-			foreach ($boxes as $k => $v) {
-				$eCount    = 1;
-				$boxes[$k] = trimsplit(',', $v);
+            // inject subplattes & selectors
+            $arrSelectors = array_intersect($GLOBALS['TL_DCA'][static::$strSpreadDca]['palettes']['__selector__'], $arrFieldKeys);
 
-				foreach ($boxes[$k] as $kk => $vv) {
-					if (preg_match('/^\[.*\]$/', $vv)) {
-						++$eCount;
-						continue;
-					}
+            if (!empty($arrSelectors))
+            {
+                $dc['palettes']['__selector__'] = array_merge(is_array($dc['palettes']['__selector__']) ? $dc['palettes']['__selector__'] : array(), $arrSelectors);
 
-					if (preg_match('/^\{.*\}$/', $vv)) {
-						unset($boxes[$k][$kk]);
-					} else {
-						if (isset($GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'][$vv])) {
-							$arrField = $GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'][$vv];
-						} else if (isset($dc['fields'][$vv])) {
-							$arrField = $dc['fields'][$vv];
-						} else{
-							continue;
-						}
+                foreach ($arrSelectors as $key)
+                {
+                    $arrFields = array_merge($arrFields, static::getPaletteFields($key, $dc, 'subpalettes'));
 
-						$arrFields[$vv] = $arrField;
+                }
 
-						// orderSRC support
-						if (isset($arrField['eval']['orderField'])) {
-							$arrFields[$arrField['eval']['orderField']] = $GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'][$arrField['eval']['orderField']];
-						}
-					}
-				}
-			}
+                $dc['subpalettes'] = array_merge(is_array($dc['subpalettes']) ? $dc['subpalettes'] : array(), $GLOBALS['TL_DCA'][static::$strSpreadDca]['subpalettes']);
+            }
 
-			// Unset a box if it does not contain any fields
-			if (count($boxes[$k]) < $eCount) {
-				unset($boxes[$k]);
-			}
-		}
+            if (!is_array($arrFields))
+            {
+                return;
+            }
 
-		return $arrFields;
-	}
+            // inject fields
+            $dc['fields'] = array_merge($arrFields, (is_array($dc['fields']) ? $dc['fields'] : array()));
+        }
+
+        \System::loadLanguageFile(static::$strSpreadDca);
+        \System::loadLanguageFile($strName);
+
+        // add language to TL_LANG palette
+        if(is_array($GLOBALS['TL_LANG'][static::$strSpreadDca]))
+        {
+            $GLOBALS['TL_LANG'][$strName] = array_merge(is_array($GLOBALS['TL_LANG'][$strName]) ? $GLOBALS['TL_LANG'][$strName] : array(), $GLOBALS['TL_LANG'][static::$strSpreadDca]);
+        }
+    }
+
+    protected static function getPaletteFields($strPalette, $dc, $type = 'palettes')
+    {
+        $boxes = trimsplit(';', $GLOBALS['TL_DCA'][static::$strSpreadDca][$type][$strPalette]);
+
+        $arrFields = array();
+
+        if (!empty($boxes))
+        {
+            foreach ($boxes as $k => $v)
+            {
+                $eCount    = 1;
+                $boxes[$k] = trimsplit(',', $v);
+
+                foreach ($boxes[$k] as $kk => $vv)
+                {
+                    if (preg_match('/^\[.*\]$/', $vv))
+                    {
+                        ++$eCount;
+                        continue;
+                    }
+
+                    if (preg_match('/^\{.*\}$/', $vv))
+                    {
+                        unset($boxes[$k][$kk]);
+                    } else
+                    {
+                        if (isset($GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'][$vv]))
+                        {
+                            $arrField = $GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'][$vv];
+                        } else
+                        {
+                            if (isset($dc['fields'][$vv]))
+                            {
+                                $arrField = $dc['fields'][$vv];
+                            } else
+                            {
+                                continue;
+                            }
+                        }
+
+                        $arrFields[$vv] = $arrField;
+
+                        // orderSRC support
+                        if (isset($arrField['eval']['orderField']))
+                        {
+                            $arrFields[$arrField['eval']['orderField']] = $GLOBALS['TL_DCA'][static::$strSpreadDca]['fields'][$arrField['eval']['orderField']];
+                        }
+                    }
+                }
+            }
+
+            // Unset a box if it does not contain any fields
+            if (count($boxes[$k]) < $eCount)
+            {
+                unset($boxes[$k]);
+            }
+        }
+
+        return $arrFields;
+    }
 
 }
